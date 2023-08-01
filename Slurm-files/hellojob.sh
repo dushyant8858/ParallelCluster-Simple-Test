07664913d8d397a7a904363c422af36670d20dfb
Dushyant Dubaria <dubaria>
2023-08-01 11:45:47 -0400

    README.md v2

--- Slurm-files/hellojob.sh
+++ Slurm-files/hellojob.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "Hello World from $(hostname)"
+echo "################################################################"
+echo "Thank you for attending Parallel Cluster training on $(date) :) "
+echo "################################################################"
 sleep 30
